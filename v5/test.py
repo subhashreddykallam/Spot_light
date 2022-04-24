eb403d53a53f39d20cc0cc3ae3481265fca3729a
skallam <subhashreddy_kallam@intuit.com>
2022-04-24 22:54:43 +0530

    movement threshold

--- v5/test.py
+++ v5/test.py
@@ -10,7 +10,7 @@ from package.handle_multi_face import handle_multi_face
 from package.super_resolution import super_resolution
 from package.get_center_frame import get_center_frame
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('input.mp4')
 detector = dlib.get_frontal_face_detector()
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -41,11 +41,13 @@ if __name__ == "__main__":
 
     def displacement(p, q):
         return abs(p[0]-q[0]) + abs(p[1]-q[1])
-
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    output = cv2.VideoWriter('spotlight_window_upscale.mp4', fourcc, 24, (1280, 720))
     while True:
         status, frame = cap.read()
         if not status:
             print("Error reading")
+            exit(-1)
             continue
         frame_height, frame_width = frame.shape[:2]
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -53,15 +55,17 @@ if __name__ == "__main__":
 
         co_ords = get_center_frame(frame_width, frame_height, sf_width, sf_height)
         
+        # output.write(frame)
         n = len(faces)
+        print(n)
         if len(faces) <= 1:
             co_ords = handle_single_face(faces, co_ords)
         else:
             co_ords = handle_multi_face(faces, co_ords)
         left, top, right, bottom = co_ords
+        print(co_ords)
         c = center(left, top, right, bottom)
         img = []
-
         if prev_center == [-1, -1]:
             prev_center = c
             sf_left, sf_top, sf_right, sf_bottom = spotlight_frame((left+right)//2, (top+bottom)//2, frame_width, frame_height, max(right-left, sf_width), max(bottom-top, sf_height))
@@ -77,14 +81,18 @@ if __name__ == "__main__":
                     sf_bottom+=(y_delta//abs(y_delta))*4
                 sf_left, sf_top, sf_right, sf_bottom = spotlight_frame((left+right)//2, (top+bottom)//2, frame_width, frame_height, max(right-left, sf_width), max(bottom-top, sf_height))
                 prev_center = c
-
+        # cv2.imshow("Without interpolation", img)
         img = frame[sf_top:sf_bottom, sf_left:sf_right]
         img = cv2.resize(img, (640, 360), interpolation=cv2.INTER_AREA)
-
+        
+        # cv2.imshow("With interpolation", img)
         cv2.rectangle(frame,(sf_left,sf_top),(sf_right,sf_bottom),(0,255,0),2)
-
-        cv2.imshow('Processed image', super_resolution.upscale(img))
+        cv2.imshow('img', img)
+        # cv2.imshow('Processed image', super_resolution.upscale(img))
         cv2.imshow('Raw Webcam', frame)
+        output.write(super_resolution.upscale(img))
+        
+        # cv2.imshow('Webcam', frame)
 
         key = cv2.waitKey(1)
         if key == 27:
